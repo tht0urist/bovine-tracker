863af0b2f5a431766bac8f2a6dfc142dd788eaa1
tht0urist <bouh.oualid@gmail.com>
2024-07-14 14:18:45 +0200

    Init backend

---
+++ bovine-tracker-backend/src/main/kotlin/io/obouh/bovine/tracker/bovine_tracker/BovineTrackerApplication.kt
@@ -0,0 +1,11 @@
+package io.obouh.bovine.tracker.bovine_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BovineTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BovineTrackerApplication>(*args)
+}
